064d2ce66ed4ae053904c60501fac047a16a8707
kolaente <k@knt.li>
2022-01-08 12:33:50 +0100

    feat: change the backup date format to make sure the backups sort properly

--- config.go
+++ config.go
@@ -65,7 +65,7 @@ func init() {
 
 func updateFullBackupPath() {
 	dumpTime = time.Now()
-	config.fullCurrentBackupPath = config.Folder + dumpTime.Format("02-01-2006_15-04-05") + "/"
+	config.fullCurrentBackupPath = config.Folder + dumpTime.Format("2006-01-02_15-04-05") + "/"
 	err := os.MkdirAll(config.fullCurrentBackupPath, 0744)
 	if err != nil {
 		log.Fatalf("Could not create backup folder: %s\n", err)
